bf976afbb6892485679957911e15314d5ab657a3
Kittipod Lambangchang <tonkittipod@gmail.com>
2024-09-07 01:17:37 +0700

    feat: authentication with google and add swagger

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 
+import { EnvModule } from '../env/env.module';
 import { UsersModule } from '../users/users.module';
 
 import { AuthController } from './auth.controller';
@@ -9,7 +10,7 @@ import { AuthService } from './auth.service';
 import { AccessTokenStrategy } from './strategies';
 
 @Module({
-  imports: [UsersModule, JwtModule, PassportModule],
+  imports: [UsersModule, JwtModule, PassportModule, EnvModule],
   controllers: [AuthController],
   providers: [AuthService, AccessTokenStrategy],
 })
---
+++ src/shared/constants/domain.ts
@@ -0,0 +1,1 @@
+export const KMITL_EMAIL_DOMAIN = 'kmitl.ac.th';
