089b3d82661c5c7ca60ad4507bb3b3143f33215b
Kittipod Lambangchang <tonkittipod@gmail.com>
2024-09-06 20:14:57 +0700

    feat: add generate token

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+
+import { EnvService } from '../env/env.service';
+import { UsersService } from '../users/users.service';
+
+import { IJwtPayload } from '@/shared/interfaces';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly envService: EnvService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  private generateAccessToken(studentId: string): string {
+    const payload: IJwtPayload = {
+      sub: studentId,
+    };
+
+    return this.jwtService.sign(payload, {
+      secret: this.envService.get('ACCESS_SECRET'),
+      expiresIn: this.envService.get('ACCESS_EXPIRES_IN'),
+    });
+  }
+}
