822a6b7b5421c1a4ecfa66d8aeb90a9b5b813fac
Kittipod Lambangchang <tonkittipod@gmail.com>
2024-09-06 10:34:05 +0700

    feat: import module

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+
+import { UsersModule } from '../users/users.module';
 
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { AccessTokenStrategy } from './strategies';
 
 @Module({
+  imports: [UsersModule, JwtModule, PassportModule],
   controllers: [AuthController],
   providers: [AuthService, AccessTokenStrategy],
 })
