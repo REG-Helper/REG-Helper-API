3c88e12b4dce12429f811f14ce003642821c385c
Kittipod Lambangchang <tonkittipod@gmail.com>
2024-09-06 10:27:36 +0700

    feat: add user service, access token strategy and access token guard

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { AccessTokenStrategy } from './strategies';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, AccessTokenStrategy],
 })
 export class AuthModule {}
---
+++ src/modules/auth/strategies/access-token.strategy.ts
@@ -0,0 +1,33 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+
+import { User } from '@prisma/client';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+import { EnvService } from '@/modules/env/env.service';
+import { UsersService } from '@/modules/users/users.service';
+import { IJwtPayload } from '@/shared/interfaces';
+
+@Injectable()
+export class AccessTokenStrategy extends PassportStrategy(Strategy, 'jwt-access-token') {
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly envService: EnvService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: envService.get('ACCESS_SECRET'),
+    });
+  }
+
+  async validate(payload: IJwtPayload): Promise<User> {
+    const user = await this.usersService.findUser({ studentId: payload.sub });
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
+    return user;
+  }
+}
