848e48f1698d4648c5e3853ee7724afb26ad47b8
Kittipod Lambangchang <tonkittipod@gmail.com>
2024-09-07 19:47:23 +0700

    feat: setup minio-client

--- src/modules/env/env.ts
+++ src/modules/env/env.ts
@@ -10,6 +10,12 @@ export const envSchema = z.object({
   DATABASE_URL: z.string().min(1),
   GOOGLE_CLIENT_ID: z.string().min(1),
   GOOGLE_CLIENT_SECRET: z.string().min(1),
+  MINIO_PORT: z.coerce.number(),
+  MINIO_ENDPOINT: z.string().min(1),
+  MINIO_BUCKET: z.string().min(1),
+  MINIO_ACCESS_KEY: z.string().min(1),
+  MINIO_SECRET_KEY: z.string().min(1),
+  MINIO_USE_SSL: z.coerce.boolean(),
 });
 
 export type Env = z.infer<typeof envSchema>;
--- src/modules/index.ts
+++ src/modules/index.ts
@@ -5,3 +5,13 @@ export * from './env/env.module';
 export * from './users/users.module';
 
 export * from './prisma/prisma.module';
+
+export * from './courses/courses.module';
+
+export * from './sections/sections.module';
+
+export * from './teachers/teachers.module';
+
+export * from './transcript/transcript.module';
+
+export * from './minio-client/minio-client.module';
