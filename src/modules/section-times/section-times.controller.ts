c69a077fb79d0f4b8aa5b15293bb1a077dd889c0
Kittipod Lambangchang <tonkittipod@gmail.com>
2024-09-18 16:53:51 +0700

    feat: get section-times, get section-times by id and delete section-times

--- src/modules/section-times/section-times.controller.ts
+++ src/modules/section-times/section-times.controller.ts
@@ -1,8 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param } from '@nestjs/common';
+import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 
+import { SectionTimeResponseDto } from './dto';
 import { SectionTimesService } from './section-times.service';
 
 @Controller('section-times')
+@ApiTags('section-times')
 export class SectionTimesController {
   constructor(private readonly sectionTimesService: SectionTimesService) {}
+
+  @Get()
+  @ApiOkResponse({
+    type: [SectionTimeResponseDto],
+  })
+  async getSectionTimes(): Promise<SectionTimeResponseDto[]> {
+    return this.sectionTimesService.getSectionTimes();
+  }
+
+  @Get(':id')
+  @ApiOkResponse({
+    type: SectionTimeResponseDto,
+  })
+  async getSectionTime(@Param('id') sectionTimeId: string): Promise<SectionTimeResponseDto> {
+    return this.sectionTimesService.getSectionTime(sectionTimeId);
+  }
+
+  @Delete(':id')
+  @ApiOkResponse({
+    type: SectionTimeResponseDto,
+  })
+  async deleteSectionTime(@Param('id') sectionTimeId: string): Promise<SectionTimeResponseDto> {
+    return this.sectionTimesService.deleteSectionTime(sectionTimeId);
+  }
 }
--- src/modules/section-times/section-times.service.ts
+++ src/modules/section-times/section-times.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+import { SectionTime } from '@prisma/client';
+
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class SectionTimesService {}
+export class SectionTimesService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getSectionTimes(): Promise<SectionTime[]> {
+    return this.prisma.sectionTime.findMany();
+  }
+
+  async getSectionTime(sectionTimeId: string): Promise<SectionTime> {
+    const sectionTime = await this.prisma.sectionTime.findUnique({
+      where: {
+        id: sectionTimeId,
+      },
+    });
+
+    if (!sectionTime) {
+      throw new NotFoundException('Section time not found');
+    }
+
+    return sectionTime;
+  }
+
+  async deleteSectionTime(sectionTimeId: string): Promise<SectionTime> {
+    await this.getSectionTime(sectionTimeId);
+
+    const deletedSectionTime = await this.prisma.sectionTime.delete({
+      where: {
+        id: sectionTimeId,
+      },
+    });
+
+    return deletedSectionTime;
+  }
+}
