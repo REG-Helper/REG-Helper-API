3e7502cb7b533f34067520d0ace3dfc91753e0bb
ThanithK <victorykung5@gmail.com>
2024-10-24 01:03:51 +0700

    fix: add min of freeelective and remove negative credits output

--- src/shared/utils/calculate-grade.ts
+++ src/shared/utils/calculate-grade.ts
@@ -17,22 +17,6 @@ export function checkRemainCourse(courses: Course[]): ICalcCourseSyllabus {
     }
   });
 
-  for (const category in syllabus) {
-    const categoryCredits = syllabus[category].courses.electiveCredits;
-
-    if (categoryCredits < 0) {
-      if (category.includes('genEd') && syllabus.gendEdElective.courses.electiveCredits > 0) {
-        syllabus.gendEdElective.courses.electiveCredits += categoryCredits;
-        syllabus.gendEdElective.remainingCredits += categoryCredits;
-      } else {
-        syllabus.freeElective.remainingCredits += categoryCredits;
-        syllabus.freeElective.courses.electiveCredits += categoryCredits;
-      }
-
-      syllabus[category].course.electiveCredits = 0;
-    }
-  }
-
   return syllabus;
 }
 
@@ -43,35 +27,65 @@ function processSpecificCourses(course: Course, syllabus: ICalcCourseSyllabus): 
     syllabus.specificCore.courses.fixedCourses.has(course.id)
   ) {
     syllabus.specificCore.courses.fixedCourses.delete(course.id);
-    syllabus.specificCore.remainingCredits -= course.credit;
+    syllabus.specificCore.remainingCredits = Math.max(
+      0,
+      syllabus.specificCore.remainingCredits - course.credit,
+    );
   } else if (
     course.subGroup == 'REQUIRED' &&
     syllabus.specificReq.courses.fixedCourses instanceof Set &&
     syllabus.specificReq.courses.fixedCourses.has(course.id)
   ) {
     syllabus.specificReq.courses.fixedCourses.delete(course.id);
-    syllabus.specificReq.remainingCredits -= course.credit;
+    syllabus.specificReq.remainingCredits = Math.max(
+      0,
+      syllabus.specificReq.remainingCredits - course.credit,
+    );
   } else if (
     course.subGroup == 'ELEC_REQ' &&
     syllabus.specificElectiveReq.courses.electiveCredits > 0
   ) {
-    syllabus.specificElectiveReq.courses.electiveCredits -= course.credit;
-    syllabus.specificElectiveReq.remainingCredits -= course.credit;
+    syllabus.specificElectiveReq.courses.electiveCredits = Math.max(
+      0,
+      syllabus.specificElectiveReq.courses.electiveCredits - course.credit,
+    );
+    syllabus.specificElectiveReq.remainingCredits = Math.max(
+      0,
+      syllabus.specificElectiveReq.remainingCredits - course.credit,
+    );
   } else if (
     course.subGroup == 'ALT_STUDY' &&
     syllabus.specificAltStudy.courses.electiveCredits > 0
   ) {
-    syllabus.specificAltStudy.courses.electiveCredits -= course.credit;
-    syllabus.specificAltStudy.remainingCredits -= course.credit;
+    syllabus.specificAltStudy.courses.electiveCredits = Math.max(
+      0,
+      syllabus.specificAltStudy.courses.electiveCredits - course.credit,
+    );
+    syllabus.specificAltStudy.remainingCredits = Math.max(
+      0,
+      syllabus.specificAltStudy.remainingCredits - course.credit,
+    );
   } else if (
     course.subGroup == 'MAJOR_ELEC' &&
     syllabus.specificMajorElective.courses.electiveCredits > 0
   ) {
-    syllabus.specificMajorElective.courses.electiveCredits -= course.credit;
-    syllabus.specificMajorElective.remainingCredits -= course.credit;
+    syllabus.specificMajorElective.courses.electiveCredits = Math.max(
+      0,
+      syllabus.specificMajorElective.courses.electiveCredits - course.credit,
+    );
+    syllabus.specificMajorElective.remainingCredits = Math.max(
+      0,
+      syllabus.specificMajorElective.remainingCredits - course.credit,
+    );
   } else {
-    syllabus.freeElective.remainingCredits -= course.credit;
-    syllabus.freeElective.courses.electiveCredits -= course.credit;
+    syllabus.freeElective.remainingCredits = Math.max(
+      0,
+      syllabus.freeElective.remainingCredits - course.credit,
+    );
+    syllabus.freeElective.courses.electiveCredits = Math.max(
+      0,
+      syllabus.freeElective.courses.electiveCredits - course.credit,
+    );
   }
 }
 
@@ -82,32 +96,59 @@ function processGenEdCourses(course: Course, syllabus: ICalcCourseSyllabus): voi
     syllabus.gedEdFundamental.courses.fixedCourses.has(course.id)
   ) {
     syllabus.gedEdFundamental.courses.fixedCourses.delete(course.id);
-    syllabus.gedEdFundamental.remainingCredits -= course.credit;
+    syllabus.gedEdFundamental.remainingCredits = Math.max(
+      0,
+      syllabus.gedEdFundamental.remainingCredits - course.credit,
+    );
   } else if (
     course.subGroup == 'LANG' &&
     syllabus.genEdLanguageCommunication.courses.fixedCourses instanceof Set &&
     syllabus.genEdLanguageCommunication.courses.fixedCourses.has(course.id)
   ) {
     syllabus.genEdLanguageCommunication.courses.fixedCourses.delete(course.id);
-    syllabus.genEdLanguageCommunication.remainingCredits -= course.credit;
+    syllabus.genEdLanguageCommunication.remainingCredits = Math.max(
+      0,
+      syllabus.genEdLanguageCommunication.remainingCredits - course.credit,
+    );
   } else if (
     course.subGroup == 'FAC_SPEC' &&
     syllabus.genEdFacultySpecific.courses.fixedCourses instanceof Set &&
     syllabus.genEdFacultySpecific.courses.fixedCourses.has(course.id)
   ) {
     syllabus.genEdFacultySpecific.courses.fixedCourses.delete(course.id);
-    syllabus.genEdFacultySpecific.remainingCredits -= course.credit;
+    syllabus.genEdFacultySpecific.remainingCredits = Math.max(
+      0,
+      syllabus.genEdFacultySpecific.remainingCredits - course.credit,
+    );
   } else if (
     course.subGroup == 'LANG' &&
     syllabus.genEdLanguageCommunication.courses.electiveCredits > 0
   ) {
-    syllabus.genEdLanguageCommunication.courses.electiveCredits -= course.credit;
-    syllabus.genEdLanguageCommunication.remainingCredits -= course.credit;
+    syllabus.genEdLanguageCommunication.courses.electiveCredits = Math.max(
+      0,
+      syllabus.genEdLanguageCommunication.courses.electiveCredits - course.credit,
+    );
+    syllabus.genEdLanguageCommunication.remainingCredits -= Math.max(
+      0,
+      syllabus.genEdLanguageCommunication.remainingCredits - course.credit,
+    );
   } else if (syllabus.gendEdElective.courses.electiveCredits > 0) {
-    syllabus.gendEdElective.courses.electiveCredits -= course.credit;
-    syllabus.gendEdElective.remainingCredits -= course.credit;
+    syllabus.gendEdElective.courses.electiveCredits = Math.max(
+      0,
+      syllabus.gendEdElective.courses.electiveCredits - course.credit,
+    );
+    syllabus.gendEdElective.remainingCredits = Math.max(
+      0,
+      syllabus.gendEdElective.remainingCredits - course.credit,
+    );
   } else {
-    syllabus.freeElective.remainingCredits -= course.credit;
-    syllabus.freeElective.courses.electiveCredits -= course.credit;
+    syllabus.freeElective.remainingCredits = Math.max(
+      0,
+      syllabus.freeElective.remainingCredits - course.credit,
+    );
+    syllabus.freeElective.courses.electiveCredits = Math.max(
+      0,
+      syllabus.freeElective.courses.electiveCredits - course.credit,
+    );
   }
 }
